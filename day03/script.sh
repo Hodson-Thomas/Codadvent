a483a56a56019663dac677da666aaf62cd746c0a
Thomas HODSON <thomas.hodson.pro@gmail.com>
2025-12-03 09:39:02 +0100

    day3

--- day03/script.sh
+++ day03/script.sh
@@ -1,9 +1,17 @@
-#!bin/bash
+#!/bin/bash
 
+echo = "🔍️ Searching for file ..."
 file="$1"
 
-chmod u+x "$file"
-
-chmod go-rwx "$file"
-
-./"$file"
+if test -f "$file"; then
+    echo "✅️ 📄 File $file found."
+    chmod u+x "$file"
+    echo "✅️ Access set for user."
+    chmod go-rwx "$file"
+    echo "✅️ Access remove for other groups and other users."
+    echo "⚙️ Loading script ..."
+    ./"$file"
+    echo "✅️ Program done."
+else
+    echo "❌️ 📄 File $file not found."
+fi
