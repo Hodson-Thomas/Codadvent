e02dc2835aa6320daf4b7a3308ef9a7a9807d1e5
Thomas HODSON <thomas.hodson.pro@gmail.com>
2025-12-10 09:54:53 +0100

    day10

--- day10/day10.cs
+++ day10/day10.cs
@@ -5,27 +5,11 @@ public static class GiftSelector
     public static string? SelectGiftFor(Child child) => child.Behavior switch
     {
         Behavior.Naughty => null,
-        Behavior.Normal when child.Age < 14 => child.GiftRequests
-            .Where(gift => gift.IsFeasible)
-            .Reverse()
-            .Select(gift => gift.GiftName)
-            .FirstOrDefault(),
-        Behavior.Normal => child.Kindness > 0.5 
-            ? child.GiftRequests
-                .Where(gift => gift.IsFeasible)
-                .Reverse()
-                .Select(gift => gift.GiftName)
-                .FirstOrDefault()
-            : null,
-        Behavior.Nice when child.Age < 14 => child.GiftRequests
-            .Where(gift => gift.IsFeasible)
-            .Select(gift => gift.GiftName)
-            .FirstOrDefault(),
-        _ => child.Kindness > 0.5
-            ? child.GiftRequests
-                .Where(gift => gift.IsFeasible)
-                .Select(gift => gift.GiftName)
-                .FirstOrDefault()
-            : null
+        Behavior.Normal => child.Age >= 14 && child.Kindness < 0.5 
+            ? null
+            : child.GiftRequests.Where(gift => gift.IsFeasible).Reverse().Select(gift => gift.GiftName).FirstOrDefault(),
+        Behavior.Nice => child.Age >= 14 && child.Kindness < 0.5
+            ? null
+            : child.GiftRequests.Where(gift => gift.IsFeasible).Select(gift => gift.GiftName).FirstOrDefault()
     };
 }
